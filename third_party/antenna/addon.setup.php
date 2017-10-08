d7be30d7f321d0fdb7190f4c2151a3c834cf845d
Matt Weinberg <matt@mattweinberg.net>
2017-10-08 17:47:22 -0400

    Bump version number

--- third_party/antenna/addon.setup.php
+++ third_party/antenna/addon.setup.php
@@ -5,7 +5,7 @@ return array(
       'author_url'  => 'http://www.VectorMediaGroup.com',
       'name'        => 'Antenna',
       'description' => 'Returns the embed code and various pieces of metadata for YouTube, Vimeo, Wistia, and Viddler Videos',
-      'version'     => '2.1.3',
+      'version'     => '2.2',
       'namespace'   => 'Vector\Antenna',
       'docs_url'    => 'https://github.com/vector/antenna'
 );
